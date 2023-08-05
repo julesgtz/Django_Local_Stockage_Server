54d41690e244d9dbc3239056413442b71e05e108
j <113105305+julesgtz@users.noreply.github.com>
2023-08-05 22:14:26 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,8 @@
 @echo off
 
 pip install -r requirements.txt
-for /f "tokens=2 delims=:" %%i in ('ipconfig ^| findstr /C:"Adresse IPv4"') do set "ip=%%i"
-set ip=%ip:~1%
-python3 admin/manage.py runserver 0.0.0.0:8000
-start http://%ip%:8000
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+start http://%NetworkIP%:8000
+python admin/manage.py makemigrations
+python admin/manage.py migrate
+python admin/manage.py runserver 0.0.0.0:8000
